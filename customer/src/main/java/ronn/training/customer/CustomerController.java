14b0690dbe742d4abc27dd54c6082b0b59ff90e1
ronn <ronnie-filth@hotmail.com>
2023-04-01 17:09:11 +0200

    Add postMapping annotation

--- customer/src/main/java/ronn/training/customer/CustomerController.java
+++ customer/src/main/java/ronn/training/customer/CustomerController.java
@@ -1,16 +1,19 @@
 package ronn.training.customer;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("api/v1/customer")
+@RequestMapping("api/v1/customers")
 public record CustomerController(CustomerService service) {
 
-    public void registerCustomer(@RequestBody CustomerRegistrationRequest cr){
+
+    @PostMapping
+     public void registerCustomer(@RequestBody CustomerRegistrationRequest cr){
       log.info("New customer registration {}", cr);
       service.register(cr);
     }
