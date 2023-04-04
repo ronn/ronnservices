3ba81f2f1c32f343be2af2be1d741c2d66433332
ronn <ronnie-filth@hotmail.com>
2023-04-04 22:03:51 +0200

    http comunication with restTemplate

--- customer/src/main/java/ronn/training/customer/CustomerService.java
+++ customer/src/main/java/ronn/training/customer/CustomerService.java
@@ -1,10 +1,12 @@
 package ronn.training.customer;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public record CustomerService(
-        CustomerRepository repo
+        CustomerRepository repo,
+        RestTemplate restTemplate
 ) {
     public void register(CustomerRegistrationRequest cr) {
         Customer c = Customer.builder()
@@ -14,6 +16,19 @@ public record CustomerService(
                 .build();
 
         //TODO check CR validity
+       repo.saveAndFlush(c);
+        //TODO check if fraudster
+        FraudCheckResponse fcResponse = restTemplate.getForObject(
+                "http://localhost:808/v1/frad-check/{customerId}",
+                FraudCheckResponse.class,
+                c.getId()
+        );
+
+        if (fcResponse.isFraudster()){
+            throw new IllegalStateException("FRAUDSTER!");
+        }
+
+        //TODO send notification
 
         repo.save(c);
     }
---
+++ customer/src/main/java/ronn/training/customer/FraudCheckResponse.java
@@ -0,0 +1,6 @@
+package ronn.training.customer;
+
+public record FraudCheckResponse(
+        Boolean isFraudster
+) {
+}
